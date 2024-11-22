717805f1474dd5e93452dd7a077b3bca83fb1509
Esraasamyzakria <esamy3955@gmail.com>
2024-11-22 18:50:25 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+function newuote(){
+    var text =['"The best revenge is massive success."','"Resentment is like drinking poison and waiting for your enemies to die."','"You miss 100% of the shots you dont take."','"Do not take life too seriously. You will not get out alive."','"It s not what happens to you, but how you react to it that matters."']
+    var name =["--Frank Sinatra","--Nelson Mandela","--Wayne Gretzy","--Elbert Hubbard","--Epictetus"]
+
+    var num =Math.floor(Math.random()* text.length)
+    document.getElementById("mydata").innerHTML=(text[num]);
+    document.getElementById("myname").innerHTML=(name[num]);
+}
